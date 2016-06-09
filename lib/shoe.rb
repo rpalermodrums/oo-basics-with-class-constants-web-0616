74b1ccdab6325ad9e393318ba77655e8666c29a8
Ryan Palermo <stix250@gmail.com>
2016-06-09 18:10:27 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,10 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
+    BRANDS << brand unless BRANDS.include?(brand)
     @brand = brand
   end
 
